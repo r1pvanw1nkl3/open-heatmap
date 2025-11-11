9b00f9de3690bb1843867f35807a5240403dced6
Chris <chrissstone@gmail.com>
2025-11-11 15:36:34 -0500

    add loop to collect data

--- main.py
+++ main.py
@@ -14,24 +14,41 @@ def fetch_311_data():
     if not APP_TOKEN:
         print("No app token found")
         return
+    
+    offset = 0
+    end_of_data = False
+    data = []
 
-    params = {
-        "$$app_token": APP_TOKEN,
-        "$order": "created_date DESC",
-        "complaint_type": "Noise"
-    }
-    try:
+    while not end_of_data:
+        params = {
+            "$$app_token": APP_TOKEN,
+            "$order": "created_date DESC",
+            "complaint_type": "Noise",
+            "$limit": 1000,
+            "$offset": offset,
+            "$where": "created_date > '2025-01-01T00:00:00' and upper(complaint_type) like '%NOISE%'"
+        }
+        try:
 
-        response = requests.get(BASE_URL, params=params)
-        response.raise_for_status() 
-        data = response.json()
-        return data
+            response = requests.get(BASE_URL, params=params)
+            response.raise_for_status()
+            new_data = response.json()
 
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP Error: {http_err}")
-        print(f"Response Content: {response.text}")
-    except requests.exceptions.RequestException as err:
-        print(f"An error occurred: {err}")
+            if len(new_data) == 0:
+                end_of_data = True
+            else:
+                print(f"{len(new_data)} new records fetched")
+                data += new_data
+                print(f"{len(data)} total records fetched")
+                offset += 1000
+
+        except requests.exceptions.HTTPError as http_err:
+            print(f"HTTP Error: {http_err}")
+            print(f"Response Content: {response.text}")
+        except requests.exceptions.RequestException as err:
+            print(f"An error occurred: {err}")
+
+    return data
 
 def main():
     requests = fetch_311_data()
